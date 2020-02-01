c8bcfdd7fd0617dcfdb4a3b61c3d1ca89c76d6dd
326550324@qq.com <326550324@qq.com>
2020-02-01 16:54:06 +0800

    save

---
+++ src/Controllers/ContentController.php
@@ -0,0 +1,21 @@
+<?php
+namespace jin123456bat\Controllers;
+
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Templates\Twig;
+
+/**
+ * Class ContentController
+ * @package HelloWorld\Controllers
+ */
+class ContentController extends Controller
+{
+	/**
+	 * @param Twig $twig
+	 * @return string
+	 */
+	public function sayHello(Twig $twig):string
+	{
+		return $twig->render('HelloWorld::content.hello');
+	}
+}
