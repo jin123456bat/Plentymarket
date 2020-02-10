1b8693d4e0809dd8a08656e03214296f936f1e5a
326550324@qq.com <326550324@qq.com>
2020-02-10 23:11:59 +0800

    Default Changelist

--- src/Providers/PlentymarketRouteServiceProvider.php
+++ src/Providers/PlentymarketRouteServiceProvider.php
@@ -16,6 +16,16 @@ class PlentymarketRouteServiceProvider extends RouteServiceProvider
 	public function map(Router $router)
 	{
 	    $router->get('/','Plentymarket\Controllers\Web\IndexController@index');
+	    $router->get('/index/about','Plentymarket\Controllers\Web\IndexController@about');
+		$router->get('/index/contact','Plentymarket\Controllers\Web\IndexController@contact');
+		$router->get('/index/faq','Plentymarket\Controllers\Web\IndexController@faq');
+		$router->get('/index/login_register','Plentymarket\Controllers\Web\IndexController@login_register');
+
+
+		$router->get('/account/index','Plentymarket\Controllers\Web\AccountController@index');
+		$router->get('/account/cart','Plentymarket\Controllers\Web\AccountController@cart');
+		$router->get('/account/checkout','Plentymarket\Controllers\Web\AccountController@checkout');
+		$router->get('/account/wishlist','Plentymarket\Controllers\Web\AccountController@wishlist');
 
 	    $router->post('/api/login','Plentymarket\Controller\Api\IndexController@login');
 		$router->post('/api/register','Plentymarket\Controller\Api\IndexController@register');
