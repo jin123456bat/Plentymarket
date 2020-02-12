8850e794a06ae1c2f8796fca4f8f32c943c2ae97
326550324@qq.com <326550324@qq.com>
2020-02-13 02:41:44 +0800

    save

--- src/Controllers/Api/IndexController.php
+++ src/Controllers/Api/IndexController.php
@@ -31,41 +31,107 @@ class IndexController extends BaseApiController
 		$this->accountService = $accountService;
 	}
 
+	// Get all
+
 	/**
 	 * @return Response
 	 */
-	public function login (): Response
+	public function index (): Response
 	{
+		return $this->response->make('index', 501);
+	}
+
+	// Post
+
+	/**
+	 * @return Response
+	 */
+	public function store (): Response
+	{
+		return $this->response->make('login', 200);
 		$email = $this->request->get('email');
 		$password = $this->request->get('password');
 
 		if (empty($email) || empty($password)) {
-			return $this->error($this->trans("ApiIndex.loginEmailOrPasswordError"));
+//			return $this->error($this->trans("ApiIndex.loginEmailOrPasswordError"));
 		}
 
 		if ($this->accountService->login($email, $password)) {
-			return $this->success($this->trans('ApiIndex.loginSuccess'));
+//			return $this->success($this->trans('ApiIndex.loginSuccess'));
 		} else {
-			return $this->error($this->trans('ApiIndex.loginEmailOrPasswordError'));
+//			return $this->error($this->trans('ApiIndex.loginEmailOrPasswordError'));
 		}
+		//return $this->response->create(null, $this->defaultCode);
+	}
+
+	// Get
+
+	/**
+	 * @param string $selector
+	 * @return Response
+	 */
+	public function show (string $selector): Response
+	{
+		return $this->response->make('index', 501);
+	}
+
+	// Put/patch
+
+	/**
+	 * @param string $selector
+	 * @return Response
+	 */
+	public function update (string $selector): Response
+	{
+		return $this->response->make('index', 501);
+	}
+
+	// Delete
+
+	/**
+	 * @param string $selector
+	 * @return Response
+	 */
+	public function destroy (string $selector): Response
+	{
+		return $this->response->make('index', 501);
 	}
 
 	/**
 	 * @return Response
 	 */
-	public function register (): Response
-	{
-		$email = $this->request->get('email');
-		$password = $this->request->get('password');
-
-		if (empty($email) || empty($password)) {
-			return $this->error($this->trans("ApiIndex.registerEmailOrPasswordError"));
-		}
-
-		if ($this->accountService->register($email, $password)) {
-			return $this->success($this->trans("ApiIndex.registerSuccess"));
-		} else {
-			return $this->error($this->trans("ApiIndex.registerEmailExist"));
-		}
-	}
+//	public function login (): Response
+//	{
+//		$email = $this->request->get('email');
+//		$password = $this->request->get('password');
+//
+//		if (empty($email) || empty($password)) {
+//			return $this->error($this->trans("ApiIndex.loginEmailOrPasswordError"));
+//		}
+//
+//		if ($this->accountService->login($email, $password)) {
+//			return $this->success($this->trans('ApiIndex.loginSuccess'));
+//		} else {
+//			return $this->error($this->trans('ApiIndex.loginEmailOrPasswordError'));
+//		}
+//	}
+//
+//	/**
+//	 * @return Response
+//	 */
+//	public function register (): Response
+//	{
+//		$email = $this->request->get('email');
+//		$password = $this->request->get('password');
+//
+//		if (empty($email) || empty($password)) {
+//			return $this->error($this->trans("ApiIndex.registerEmailOrPasswordError"));
+//		}
+//
+//		if ($this->accountService->register($email, $password)) {
+//			return $this->success($this->trans("ApiIndex.registerSuccess"));
+//		} else {
+//			return $this->error($this->trans("ApiIndex.registerEmailExist"));
+//		}
+//	}
 }
