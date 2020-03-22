0548a16aacea2e2a31396bf31e17d753fbed05e6
326550324@qq.com <326550324@qq.com>
2020-03-22 19:12:44 +0800

    aaa11

--- src/Controllers/Api/AddressController.php
+++ src/Controllers/Api/AddressController.php
@@ -2,6 +2,7 @@
 
 namespace Plentymarket\Controllers\Api;
 
+use Plenty\Exceptions\ValidationException;
 use Plenty\Plugin\Http\Response;
 use Plentymarket\Controllers\BaseApiController;
 use Plentymarket\Services\AddressService;
@@ -19,19 +20,25 @@ class AddressController extends BaseApiController
 	function create (): Response
 	{
 		$address = pluginApp(AddressService::class);
-		$address = $address->create([
-			'name2' => $this->request->input('first_name'),
-			'name3' => $this->request->input('second_name'),
-			'countryId' => $this->request->input('country'),
-			'address1' => $this->request->input('address1'),
-			'address2' => $this->request->input('address2'),
-			'postalCode' => $this->request->input('zipcode'),
-			'town' => $this->request->input('town'),
-			'stateId' => $this->request->input('state'),//浙江的ID
-			'companyName' => $this->request->input('company_name'),
-			'email' => $this->request->input('email'),
-			'phone' => $this->request->input('phone')
-		]);
+		try {
+			$address = $address->create([
+				'name2' => $this->request->input('first_name'),
+				'name3' => $this->request->input('second_name'),
+				'countryId' => $this->request->input('country'),
+				'address1' => $this->request->input('address1'),
+				'address2' => $this->request->input('address2'),
+				'postalCode' => $this->request->input('zipcode'),
+				'town' => $this->request->input('town'),
+				'stateId' => $this->request->input('state'),//浙江的ID
+				'companyName' => $this->request->input('company_name'),
+				'email' => $this->request->input('email'),
+				'phone' => $this->request->input('phone')
+			]);
+		} catch (ValidationException $e) {
+			return $this->error($e->getMessage());
+		} catch (\Throwable $e) {
+			return $this->exception($e);
+		}
 
 		return $this->success($address->toArray());
 	}
