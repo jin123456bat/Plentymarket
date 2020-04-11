51088b1b34fad85cadc93849f578cbb781f026c3
326550324@qq.com <326550324@qq.com>
2020-04-12 02:01:35 +0800

    aaa11

--- src/Services/PayPalService.php
+++ src/Services/PayPalService.php
@@ -88,41 +88,44 @@ class PayPalService
 		$notify_url = AuthGuard::getUrl('/api/payment/paypal');//支付成功异步通知地址
 		$cancel_return = $return;//取消支付跳转地址
 
-		$str = '<form method="post" id="form" name="form" action="' . $this->web . '/cgi-bin/webscr&pal=V4T754QB63XXL"><input type="hidden" name="cmd" value="_cart" />
-		  <input type="hidden" name="upload" value="1" />
-		  <input type="hidden" name="business" value="info@mercuryliving.it" />';
+		$str = '<form method="post" id="form" name="form" action="' . $this->web . '/cgi-bin/webscr&pal=V4T754QB63XXL">
+		<input type="hidden" name="cmd" value="_cart" />
+	    <input type="hidden" name="upload" value="1" />
+	    <input type="hidden" name="business" value="info@mercuryliving.it" />';
 		foreach ($order->orderItems as $key => $item) {
 			$amount = $this->getAmount($item['amounts']);
-
 			$str .= '<input type="hidden" name="item_name_' . ($key + 1) . '" value="' . $item['orderItemName'] . '" />
-				  <input type="hidden" name="item_number_' . ($key + 1) . '" value="" />
-				  <input type="hidden" name="amount_' . ($key + 1) . '" value="' . $amount['priceGross'] * (1 + $item['vatRate'] / 100) . '" />
-				  <input type="hidden" name="quantity_' . ($key + 1) . '" value="' . $item['quantity'] . '" />
-				  <input type="hidden" name="weight_' . ($key + 1) . '" value="0" />';
+			  <input type="hidden" name="item_number_' . ($key + 1) . '" value="' . $item['orderItemName'] . '" />
+			  <input type="hidden" name="amount_' . ($key + 1) . '" value="' . $amount['priceGross'] * (1 + $item['vatRate'] / 100) . '" />
+			  <input type="hidden" name="quantity_' . ($key + 1) . '" value="' . $item['quantity'] . '" />
+			  <input type="hidden" name="weight_' . ($key + 1) . '" value="0" />';
 		}
 
 		$str .= '<input type="hidden" name="currency_code" value="' . $this->getCurrency() . '" />
-		  <input type="hidden" name="address_override" value="0" />
-		  <input type="hidden" name="rm" value="2" />
-		  <input type="hidden" name="no_note" value="1" />
-		  <input type="hidden" name="no_shipping" value="1" />
-		  <input type="hidden" name="charset" value="utf-8" />
-		  <input type="hidden" name="custom" value="' . $order->id . '" />
-		  <input type="hidden" name="return" value="' . $return . '" />
-		  <input type="hidden" name="notify_url" value="' . $notify_url . '" />
-		  <input type="hidden" name="cancel_return" value="' . $cancel_return . '" />
-		  <input type="submit" id="submit" value="submit">
-		  </form>
-		  <script>document.getElementById("submit").click();</script>';
-
-//		$url = 'https://www.paypal.com/cgi-bin/webscr&pal=V4T754QB63XXL';
-//		$url = 'https://www.paypal.com/cgi-bin/webscr&pal='.$pal;
-//		$param = '';
-//		foreach ($data as $key => $value) {
-//			$param .= '<input type="hidden" name="' . $key . '" value="' . $value . '">';
-//		}
-//		$param .= '<input type="submit" id="submit" value="submit"><script>document.getElementById(\'form\').submit();</script>';
-//		$str = '<form id="form" action="' . $url . '" method="post">' . $param . '</form>';
+		<input type="hidden" name="first_name" value="" />
+		<input type="hidden" name="last_name" value="" />
+		<input type="hidden" name="address1" value="" />
+		<input type="hidden" name="address2" value="" />
+		<input type="hidden" name="city" value="" />
+		<input type="hidden" name="zip" value="" />
+		<input type="hidden" name="country" value="" />
+		<input type="hidden" name="address_override" value="0" />
+		<input type="hidden" name="email" value="326550324@qq.com" />
+		<input type="hidden" name="invoice" value="201 -  " />
+		<input type="hidden" name="lc" value="it-it" />
+		<input type="hidden" name="rm" value="2" />
+		<input type="hidden" name="no_note" value="1" />
+		<input type="hidden" name="no_shipping" value="1" />
+		<input type="hidden" name="charset" value="utf-8" />
+		<input type="hidden" name="paymentaction" value="sale" />
+		<input type="hidden" name="custom" value="' . $order->id . '" />
+		<input type="hidden" name="return" value="' . $return . '" />
+		<input type="hidden" name="notify_url" value="' . $notify_url . '" />
+		<input type="hidden" name="cancel_return" value="' . $cancel_return . '" />
+		<input type="hidden" name="bn" value="OpenCart_2.0_WPS" />
+		<input type="submit" id="submit" value="submit">
+		</form>
+		<script>document.getElementById("submit").click();</script>';
 		exit($str);
 	}
 
