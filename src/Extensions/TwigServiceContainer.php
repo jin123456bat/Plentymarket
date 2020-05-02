4d52812576b82d07be22bce37883c8be48fefd69
326550324@qq.com <326550324@qq.com>
2020-05-03 05:31:21 +0800

    aaa11

--- src/Extensions/TwigServiceContainer.php
+++ src/Extensions/TwigServiceContainer.php
@@ -3,11 +3,17 @@
 namespace Plentymarket\Extensions;
 
 use Plenty\Plugin\Http\Request;
+use Plentymarket\Services\ConfigService;
 
 class TwigServiceContainer
 {
 	public function getRequst (): Request
 	{
 		return pluginApp(Request::class);
+	}
+
+	public function getConfig ()
+	{
+		return pluginApp(ConfigService::class);
 	}
 }
